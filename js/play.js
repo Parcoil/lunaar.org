a2178608df26824f3eb2c7006d5d00e872d1a146
Thedogecraft <dantaealt@gmail.com>
2023-12-06 14:45:47 -0800

    Update play.js

--- js/play.js
+++ js/play.js
@@ -12,11 +12,7 @@
             document.title = `${gameData.name} | Nativegames`;
             document.getElementById('gamename').innerText = gameData.name;
             document.getElementById('favicon').href = gameData.image;
-<<<<<<< Updated upstream
-            document.getElementById('gameIframe').src = `https://gms-zeta.vercel.app/${gameUrl}/`;
-=======
             document.getElementById('gameIframe').src = `https://gms.speedpl.us/${gameUrl}/`;
->>>>>>> Stashed changes
         })
         .catch(error => console.error('Error fetching game data:', error));
 });
