5965a18b69b09f9b8ed9d5aab3ae6071457b5d78
Thedogecraft <dantaealt@gmail.com>
2023-12-07 15:31:27 -0800

    🎮 New Games
    - Added Rocket Bot Royale
    - Added Super Mario 63
    - Thing thing arena
    - Updated games.js

--- js/games.js
+++ js/games.js
@@ -19,31 +19,42 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch((error) => console.log(error));
 
-    function displayGames(games) {
-        gameContainer.innerHTML = "";
-
-        games.forEach((game) => {
-            const gameCard = document.createElement("div");
-            gameCard.classList.add("gameCard");
-
-            const gameImg = document.createElement("img");
-            gameImg.src = game.image;
-            if (game.name === "Request a Game") {
-                gameImg.addEventListener("click", function () {
-                    window.location.href = game.url;
-                });
-            } else {
-                gameImg.addEventListener("click", function () {
-                    window.location.href = '/play?game=' + encodeURIComponent(game.url);
-                });
-            }
-            gameCard.appendChild(gameImg);
-
-            const gameTitle = document.createElement("h3");
-            gameTitle.textContent = game.name;
-            gameCard.appendChild(gameTitle);
-
-            gameContainer.appendChild(gameCard);
-        });
-    }
-});
+        function displayGames(games) {
+            gameContainer.innerHTML = "";
+        
+            games.forEach((game) => {
+                const gameCard = document.createElement("div");
+                gameCard.classList.add("gameCard");
+        
+                const gameImg = document.createElement("img");
+                gameImg.src = game.image;
+                if (game.name === "Request a Game") {
+                    gameImg.addEventListener("click", function () {
+                        window.location.href = game.url;
+                    });
+                } else {
+                    gameImg.addEventListener("click", function () {
+                        window.location.href = '/play?game=' + encodeURIComponent(game.url);
+                    });
+                }
+                gameCard.appendChild(gameImg);
+        
+                const gameTitle = document.createElement("h3");
+                gameTitle.textContent = game.name;
+        
+                // Check if game is new and add 'New' badge with spaces
+                if (game.new) {
+                    const newBadge = document.createElement("span");
+                    newBadge.classList.add("badge");
+                    newBadge.textContent = "New";
+        
+                    // Add two blank spaces before the badge
+                    gameTitle.appendChild(document.createTextNode("\u00A0\u00A0"));
+                    gameTitle.appendChild(newBadge);
+                }
+        
+                gameCard.appendChild(gameTitle);
+        
+                gameContainer.appendChild(gameCard);
+            });
+        }})
