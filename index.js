054687dd783ba718f6f8340dfb02effe23a970bd
Thedogecraft <dantaealt@gmail.com>
2024-01-20 18:52:32 -0700

    removed junk

--- index.js
+++ index.js
@@ -76,26 +76,6 @@ app.get("/sitemap.gay", (req, res) => {
   });
 });
 
-app.get("/games.lol", (req, res) => {
-  const filePath = path.join(publicPath, "games.json");
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(404).send("Games not found");
-    }
-  });
-});
-
-app.get("/games1", (req, res) => {
-  const filePath = path.join(__dirname, "games.json");
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(404).send("Games not found");
-    }
-  });
-});
-
 // Handle clean URLs
 app.get("/:page", (req, res) => {
   const page = req.params.page;
