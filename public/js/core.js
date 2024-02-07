6d20f89c922c453a5ba4de67d750d84e1322d418
gamfam1 <159219920+gamfam1@users.noreply.github.com>
2024-02-06 22:37:44 -0500

    search is less likely to be detected

--- public/js/core.js
+++ public/js/core.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
               <li><a href="/./bookmarklets">Bookmarklets</a></li>
               <li><a href="/./apps">Apps</a></li>
               <li><a href="/./form">Request</a></li>
-              <li><a href="/./search">Pr0x</a></li>
+              <li><a href="/./search">Search</a></li>
               <li><a href="#" onclick="createBlank();">about:blank</a></li>
               <li><a href="/./settings">Settings</a></li>
               <li><a href="/./other/">More</a></li>
