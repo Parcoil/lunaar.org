c2b757d47980b4d14a015d088725e1829f1ea3b5
Thedogecraft <dantaealt@gmail.com>
2025-03-15 15:46:23 -0700

    🐛 6.3.1
    - Fixed Games not working
    - Added Animation to settings page

--- public/js/games.js
+++ public/js/games.js
@@ -1,54 +1,133 @@
 async function fetchGames() {
-  try {
-    const response = await fetch("./json/games.json");
-    const games = await response.json();
-    window.gamesData = games;
+	try {
+		const response = await fetch("./json/games.json");
+		const games = await response.json();
+		window.gamesData = games;
 
-    displayGames(games);
-    updateSearchBarPlaceholder(games.length);
-  } catch (error) {
-    console.error("Error fetching games:", error);
-  }
+		displayGames(games);
+		updateSearchBarPlaceholder(games.length);
+	} catch (error) {
+		console.error("Error fetching games:", error);
+	}
 }
 
 function displayGames(games) {
-  const gamesContainer = document.getElementById("game-container");
-  gamesContainer.innerHTML = "";
+	const gamesContainer = document.getElementById("game-container");
+	gamesContainer.innerHTML = "";
 
-  games.forEach((game) => {
-    const gameDiv = document.createElement("div");
-    gameDiv.className = "game fade-in";
+	games.forEach((game) => {
+		const gameDiv = document.createElement("div");
+		gameDiv.className = "game fade-in";
+		const gameImage = game.proxy
+			? `/media/games/${game.image}`
+			: `/cdn/${game.url}/${game.image}`;
 
-    const gameImage = game.proxy
-      ? `/media/games/${game.image}`
-      : `/cdn/${game.url}/${game.image}`;
-
-    gameDiv.innerHTML = `
+		gameDiv.innerHTML = `
         <img src="${gameImage}" alt="${game.name}" loading="lazy" width="200" height="200" />
         <p>${game.name}</p>
       `;
 
-    gamesContainer.appendChild(gameDiv);
-  });
+		if (game.new) {
+			gameDiv.querySelector("p").innerHTML += ' <span class="badge">New</span>';
+		}
+		if (game.top) {
+			gameDiv.querySelector("p").innerHTML +=
+				' <span class="badge">Hot 🔥</span>';
+		}
+		if (game.exp) {
+			gameDiv.querySelector("p").innerHTML += ' <span class="badge">🧪</span>';
+		}
+		if (game.updated) {
+			gameDiv.querySelector("p").innerHTML +=
+				' <span class="badge">🆕 Updated</span>';
+		}
+		const imageElement = gameDiv.querySelector("img");
+		imageElement.addEventListener("click", () => {
+			if (game.proxy) {
+				sessionStorage.setItem(
+					"lpurl",
+					__uv$config.prefix + __uv$config.encodeUrl(game.url),
+				);
+				sessionStorage.setItem("rawurl", `${game.url}`);
+				window.location.href = "/./go";
+			} else {
+				window.location.href = `./play?game=${game.url}`;
+			}
+			if (game.exp) {
+				alert("this game is experimental 🧪");
+			}
+		});
+		gamesContainer.appendChild(gameDiv);
+	});
 }
 
 function searchGames(searchTerm) {
-  const filteredGames = window.gamesData.filter((game) =>
-    game.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+	const filteredGames = window.gamesData.filter((game) =>
+		game.name.toLowerCase().includes(searchTerm.toLowerCase()),
+	);
 
-  displayGames(filteredGames);
-  updateSearchBarPlaceholder(filteredGames.length);
+	displayGames(filteredGames);
+	updateSearchBarPlaceholder(filteredGames.length);
 }
 
 function updateSearchBarPlaceholder(count) {
-  document.getElementById(
-    "search-input"
-  ).placeholder = `Search for ${count} games`;
+	document.getElementById("search-input").placeholder =
+		`Search for ${count} games`;
 }
 
 document.getElementById("search-input").addEventListener("input", (event) => {
-  searchGames(event.target.value);
+	searchGames(event.target.value);
 });
 
-document.addEventListener("DOMContentLoaded", fetchGames);
+const notifications = [
+	'Have a issue? create a ticket in our <a href="https://discord.gg/parcoil" class="link">Discord Server</a>',
+	'Try our our other proxy <a href="https://gostarlight.xyz" class="link">Starlight</a>',
+	'Dont forget to star lunaar on <a href="https://github.com/Parcoil/lunaar" class="link">Github</a>',
+	'Join the <a href="https://discord.gg/parcoil" class="link">Discord Server</a>',
+	"🧀",
+	"idk what to put here",
+	'GTA 6 FREE <a class="link" href="/uv/service/hvtrs8%2F-wuw%2Cymuvu%60e%2Ccmm-wctah%3Dv%3FxtFXjm5RgE0">DOWNLOAD</a>',
+];
+
+function getRandomNotification() {
+	const index = Math.floor(Math.random() * notifications.length);
+	return notifications[index];
+}
+
+function showNotification() {
+	const notificationDiv = document.getElementById("notification");
+	if (!notificationDiv) return;
+
+	const message = getRandomNotification();
+	notificationDiv.innerHTML = `
+    <i class="fa-solid fa-circle-info"></i>
+    &nbsp;
+      ${message}
+      <span class="close-btn">&times;</span>
+    `;
+	notificationDiv.classList.remove("hide");
+	notificationDiv.classList.add("show");
+}
+
+function hideNotification() {
+	const notificationDiv = document.getElementById("notification");
+	if (!notificationDiv) return;
+
+	notificationDiv.classList.remove("show");
+	notificationDiv.classList.add("hide");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const notificationDiv = document.getElementById("notification");
+
+	notificationDiv.addEventListener("click", (e) => {
+		if (e.target.classList.contains("close-btn")) {
+			hideNotification();
+		}
+	});
+
+	showNotification();
+	setInterval(showNotification, 15000);
+
+	fetchGames();
+});
--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,12 @@
 document.body.style.overflow = "hidden";
 
 const texts = [
-  "Lunaar",
-  "Freedom",
-  "Perfection",
-  "The Future",
-  "The Ultimate Proxy",
-  "The best site!",
+	"Lunaar",
+	"Freedom",
+	"Perfection",
+	"The Future",
+	"The Ultimate Proxy",
+	"The best site!",
 ];
 const typingSpeed = 100;
 const delayBetweenTexts = 2000;
@@ -18,26 +18,26 @@ const typingTextElement = document.getElementById("typing-text");
 const cursorElement = document.querySelector(".cursor");
 
 function typeText() {
-  const currentText = texts[currentTextIndex];
-  if (currentCharIndex < currentText.length) {
-    typingTextElement.textContent += currentText[currentCharIndex];
-    currentCharIndex++;
-    setTimeout(typeText, typingSpeed);
-  } else {
-    setTimeout(eraseText, delayBetweenTexts);
-  }
+	const currentText = texts[currentTextIndex];
+	if (currentCharIndex < currentText.length) {
+		typingTextElement.textContent += currentText[currentCharIndex];
+		currentCharIndex++;
+		setTimeout(typeText, typingSpeed);
+	} else {
+		setTimeout(eraseText, delayBetweenTexts);
+	}
 }
 
 function eraseText() {
-  const currentText = texts[currentTextIndex];
-  if (currentCharIndex > 0) {
-    typingTextElement.textContent = currentText.slice(0, currentCharIndex - 1);
-    currentCharIndex--;
-    setTimeout(eraseText, typingSpeed);
-  } else {
-    currentTextIndex = (currentTextIndex + 1) % texts.length;
-    setTimeout(typeText, typingSpeed);
-  }
+	const currentText = texts[currentTextIndex];
+	if (currentCharIndex > 0) {
+		typingTextElement.textContent = currentText.slice(0, currentCharIndex - 1);
+		currentCharIndex--;
+		setTimeout(eraseText, typingSpeed);
+	} else {
+		currentTextIndex = (currentTextIndex + 1) % texts.length;
+		setTimeout(typeText, typingSpeed);
+	}
 }
 
 typeText();
