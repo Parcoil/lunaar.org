416e262885ad96109048e0159bf852b94ebe507f
Thedogecraft <dantaealt@gmail.com>
2025-03-19 21:51:41 -0700

    ✨ 6.4.0
    - Added icons to various pages
    - Updated items on search.html
    - Added 7 Games
    - Added Error message for the games and apps page
    -Added Blue Theme

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,50 +1,56 @@
 fetch("/json/apps.json")
-	.then((response) => response.json())
-	.then((apps) => {
-		function renderApps(filteredApps) {
-			const appContainer = document.getElementById("app-container");
-			appContainer.innerHTML = "";
+  .then((response) => response.json())
+  .then((apps) => {
+    function renderApps(filteredApps) {
+      const appContainer = document.getElementById("app-container");
+      appContainer.innerHTML = "";
 
-			filteredApps.forEach((data) => {
-				const appDiv = document.createElement("div");
-				appDiv.classList.add("app", "fade-in");
+      if (!filteredApps || filteredApps.length === 0) {
+        const html = `<div><i class="fa-solid fa-circle-exclamation big-icon"></i><h1>No Apps found. </h1></div>`;
+        appContainer.innerHTML = html;
+        return;
+      }
 
-				const img = document.createElement("img");
-				img.src = data.image;
-				img.alt = data.name;
+      filteredApps.forEach((data) => {
+        const appDiv = document.createElement("div");
+        appDiv.classList.add("app", "fade-in");
 
-				const p = document.createElement("p");
-				p.textContent = data.name;
+        const img = document.createElement("img");
+        img.src = data.image;
+        img.alt = data.name;
 
-				img.onclick = function () {
-					sessionStorage.setItem(
-						"lpurl",
-						__uv$config.prefix + __uv$config.encodeUrl(data.url),
-					);
-					sessionStorage.setItem("rawurl", data.url);
-					location.href = "/go/";
-				};
+        const p = document.createElement("p");
+        p.textContent = data.name;
 
-				appDiv.appendChild(img);
-				appDiv.appendChild(p);
-				appContainer.appendChild(appDiv);
-			});
-		}
+        img.onclick = function () {
+          sessionStorage.setItem(
+            "lpurl",
+            __uv$config.prefix + __uv$config.encodeUrl(data.url)
+          );
+          sessionStorage.setItem("rawurl", data.url);
+          location.href = "/go/";
+        };
 
-		renderApps(apps);
+        appDiv.appendChild(img);
+        appDiv.appendChild(p);
+        appContainer.appendChild(appDiv);
+      });
+    }
 
-		document
-			.getElementById("search-input")
-			.addEventListener("input", function () {
-				const query = this.value.toLowerCase();
-				const filteredApps = apps.filter((app) =>
-					app.name.toLowerCase().includes(query),
-				);
-				renderApps(filteredApps);
-			});
-	})
-	.catch((error) => {
-		console.error("Error fetching the apps data:", error);
-		document.getElementById("app-container").innerHTML =
-			"<p>Error loading apps data.</p>";
-	});
+    renderApps(apps);
+
+    document
+      .getElementById("search-input")
+      .addEventListener("input", function () {
+        const query = this.value.toLowerCase();
+        const filteredApps = apps.filter((app) =>
+          app.name.toLowerCase().includes(query)
+        );
+        renderApps(filteredApps);
+      });
+  })
+  .catch((error) => {
+    console.error("Error fetching the apps data:", error);
+    document.getElementById("app-container").innerHTML =
+      "<p>Error loading apps data.</p>";
+  });
